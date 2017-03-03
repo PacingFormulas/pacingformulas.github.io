48b7049cefa8a68ccae43810f98ba1a4e8d16195
PacingFormulas <pacingformulas@gmail.com>
2017-03-03 00:14:04 -0500

    Update and rename js-effect to effects.js

---
+++ js/effects.js
@@ -0,0 +1,73 @@
+function filterfn() {
+    var input, filter, ul, line, a, i, j;
+    var chap1=0;
+    input = document.getElementById("myInput");
+    //filter = input.value.toUpperCase(); //So all text to Upper Case
+    
+    filter = input.value; //For high-lighting text
+    
+    ul = document.getElementById("myUL");
+    line = ul.getElementsByTagName("div");
+    
+    for (i = 0; i < line.length; i++) {
+        content = line[i].getElementsByClassName("filter")[0];
+        if (content.innerHTML.lastIndexOf(filter) > -1) { //Find if a formula contain the text
+            
+            
+    text = line[i].getElementsByClassName("body");
+            var once=0;
+    for (j = 0; j < text.length; j++) {
+        if( once==0) {
+        text[j].innerHTML= text[j].innerHTML.replace("<span style=\"color:red;\">", ""); //Remove previous effect
+        text[j].innerHTML= text[j].innerHTML.replace("</span>", "");
+        
+            text[j].innerHTML= text[j].innerHTML.replace(filter, "<span style=\"color:red;\">"+filter+"</span>");
+        }
+    }
+            
+            line[i].style.display = "";
+            
+            
+        } else {
+            
+          line[i].style.display = "none";
+            
+           // if (i==1) {chap1++};
+           // if (i==2) {chap1++};
+           // if (chap1!=2) {line[0].style.display = "";} else {line[0].style.display = "none";}
+            
+            
+
+        }
+    }
+}
+    
+function filtervalue(letter) {
+    
+    if (letter>0) {
+    if (letter==3) {
+        text = "γ"
+    } else if (letter==4)  {
+        text = "δ"
+    } else if (letter==5)  {
+        text = "ε"
+    } else if (letter==8)  {
+        text = "θ"
+    } else if (letter==16)  {
+        text = "π"
+    } else if (letter==18)  {
+        text = "σ"
+    } else if (letter==21)  {
+        text = "Φ"
+    } else if (letter==24)  {
+        text = "ω"
+    } 
+    input = document.getElementById("myInput");
+    input.value = text;
+    } else {
+    input = document.getElementById("myInput");
+    input.value = "";
+    }
+    
+filterfn()
+}
